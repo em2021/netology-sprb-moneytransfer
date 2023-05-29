3c3c850f4a73fb35b1a6a8dd4fae5fb9f95a0ecc
em2021 <egor_m@icloud.com>
2023-05-30 01:16:55 +0300

    Fixed CardRepository class

--- src/main/java/ru/netology/moneytransfer/repository/CardRepository.java
+++ src/main/java/ru/netology/moneytransfer/repository/CardRepository.java
@@ -3,24 +3,26 @@ package ru.netology.moneytransfer.repository;
 import org.springframework.stereotype.Repository;
 import ru.netology.moneytransfer.model.Card;
 
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 @Repository
 public class CardRepository {
 
-    private ConcurrentMap<String, Card> validCards = init();
+    private Map<String, Card> validCards = new ConcurrentHashMap<>();
+
+    public CardRepository() {
+        init();
+    }
 
     public Card getCard(String number) {
         return validCards.get(number);
     }
 
-    private ConcurrentMap<String, Card> init() {
-        ConcurrentMap<String, Card> cards = new ConcurrentHashMap<>();
+    private void init() {
         Card card1 = new Card("1111111111111111", "12", "2023", "123");
         Card card2 = new Card("2222222222222222", "12", "2023", "321");
-        cards.put(card1.getNumber(), card1);
-        cards.put(card2.getNumber(), card2);
-        return cards;
+        validCards.put(card1.getNumber(), card1);
+        validCards.put(card2.getNumber(), card2);
     }
 }
