c3c797a47eeab8554cfc03a08085e51ae33bc9e3
em2021 <egor_m@icloud.com>
2023-06-01 00:14:03 +0300

    Updated unit tests for MoneyTransferServiceImpl

--- src/test/java/ru/netology/moneytransfer/service/MoneyTransferServiceImplTests.java
+++ src/test/java/ru/netology/moneytransfer/service/MoneyTransferServiceImplTests.java
@@ -1,41 +1,42 @@
 package ru.netology.moneytransfer.service;
 
 import org.junit.jupiter.api.*;
-import org.mockito.InjectMocks;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import ru.netology.moneytransfer.exception.InvalidInput;
-import ru.netology.moneytransfer.exception.TransferNotConfirmed;
-import ru.netology.moneytransfer.model.Amount;
-import ru.netology.moneytransfer.model.Card;
-import ru.netology.moneytransfer.model.TransferData;
+import ru.netology.moneytransfer.exception.OperationNotConfirmed;
+import ru.netology.moneytransfer.model.*;
 import ru.netology.moneytransfer.repository.CardRepository;
 import ru.netology.moneytransfer.repository.OperationIdRepository;
 
+import java.util.stream.Stream;
+
 public class MoneyTransferServiceImplTests {
 
-    //TODO: Review field declaration
+    private MoneyTransferService moneyTransferServiceImpl;
     @Mock
     private CardRepository cardRepositoryImpl;
     @Mock
     private OperationIdRepository operationIdRepositoryImpl;
     @Mock
-    private CardValidationService cardValidationService;
-    @InjectMocks
-    private MoneyTransferService moneyTransferServiceImpl =
-            new MoneyTransferServiceImpl(cardRepositoryImpl, operationIdRepositoryImpl, cardValidationService);
+    private CardValidationService cardValidationServiceImp;
 
     @BeforeAll
     public static void beforeAll() {
         System.out.println("MoneyTransferServiceImplTests tests started");
     }
 
-    //TODO: Fix test setup
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.openMocks(this);
-        System.out.println("MoneyTransferServiceTests test started");
+        moneyTransferServiceImpl = new MoneyTransferServiceImpl(cardRepositoryImpl,
+                operationIdRepositoryImpl,
+                cardValidationServiceImp);
+        System.out.println("MoneyTransferServiceImplTests test started");
     }
 
     @AfterEach
@@ -49,119 +50,84 @@ public class MoneyTransferServiceImplTests {
     }
 
     @Test
-    public void testMakeTransfer_whenSameCards_throwException() {
+    public void testMakeTransfer_whenCardIsValid_returnOperationId() {
         //given:
         Amount amount = new Amount(100, "RUR");
-        TransferData transferData = new TransferData("1234123412341234",
+        TransferData transferData = new TransferData("1111111111111111",
                 "12/23",
                 "123",
-                "1234123412341234",
+                "2222222222222222",
                 amount);
+        Card card = new Card("1111111111111111", "12", "2023", "123");
+        Mockito.when(cardRepositoryImpl.getCard(Mockito.startsWith("1")))
+                .thenReturn(card);
+        Mockito.when(cardValidationServiceImp.cardIsValid(transferData, card))
+                .thenReturn(true);
+        //when:
+        OperationId actual = moneyTransferServiceImpl.makeTransfer(transferData);
         //then:
-        Assertions.assertThrows(TransferNotConfirmed.class, () -> {
-            //when:
-            moneyTransferServiceImpl.makeTransfer(transferData);
-        });
+        Assertions.assertNotNull(actual);
     }
 
-    @Test
-    public void testMakeTransfer_whenCardDoesntExist_throwException() {
+    @ParameterizedTest
+    @MethodSource("sourceForConfirmOperation")
+    public void testConfirmOperation_whenConfirmationDataOrCodeIsNull_thenThrowException(Code code,
+                                                                                         OperationId opId) {
         //given:
-        Amount amount = new Amount(100, "RUR");
-        TransferData transferData = new TransferData("1234123412341234",
-                "12/23",
-                "123",
-                "4567456745674567",
-                amount);
-        Mockito.when(cardRepositoryImpl.getCard(transferData.getCardFromNumber()))
-                .thenReturn(null);
+        ConfirmationData confirmationData = Mockito.mock(ConfirmationData.class);
+        Mockito.when(confirmationData.getOperationId())
+                .thenReturn(opId);
+        Mockito.when(confirmationData.getCode())
+                .thenReturn(code);
         //then:
         Assertions.assertThrows(InvalidInput.class, () -> {
             //when:
-            moneyTransferServiceImpl.makeTransfer(transferData);
+            moneyTransferServiceImpl.confirmOperation(confirmationData);
         });
     }
 
     @Test
-    public void testMakeTransfer_whenExpirationDateDoesntMatch_throwException() {
+    public void testConfirmOperation_whenCodeIsNotNull_thenDontThrowException() {
         //given:
-        Amount amount = new Amount(100, "RUR");
-        TransferData transferData = new TransferData("1234123412341234",
-                "12/23",
-                "123",
-                "4567456745674567",
-                amount);
-        Card card = new Card("1234123412341234", "12", "2024", "123");
-        Mockito.when(cardRepositoryImpl.getCard(transferData.getCardFromNumber()))
-                .thenReturn(card);
-        //then:
-        Assertions.assertThrows(InvalidInput.class, () -> {
-            //when:
-            moneyTransferServiceImpl.makeTransfer(transferData);
-        });
-    }
-
-    @Test
-    public void testMakeTransfer_whenCvvDoesntMatch_throwException() {
-        //given:
-        Amount amount = new Amount(100, "RUR");
-        TransferData transferData = new TransferData("1234123412341234",
-                "12/23",
-                "123",
-                "4567456745674567",
-                amount);
-        Card card = new Card("1234123412341234", "12", "2023", "124");
-        Mockito.when(cardRepositoryImpl.getCard(transferData.getCardFromNumber()))
-                .thenReturn(card);
-        //then:
-        Assertions.assertThrows(InvalidInput.class, () -> {
-            //when:
-            moneyTransferServiceImpl.makeTransfer(transferData);
-        });
-    }
-
-    @Test
-    public void testMakeTransfer_whenCardExistsAndValid_thenDontThrowException() {
-        //given:
-        Amount amount = new Amount(100, "RUR");
-        TransferData transferData = new TransferData("1234123412341234",
-                "12/23",
-                "123",
-                "4567456745674567",
-                amount);
-        Card card = new Card("1234123412341234", "12", "2023", "123");
-        Mockito.when(cardRepositoryImpl.getCard(transferData.getCardFromNumber()))
-                .thenReturn(card);
+        OperationId opId = new OperationId("111");
+        Code code = new Code("0000");
+        ConfirmationData confirmationData = Mockito.mock(ConfirmationData.class);
+        Mockito.when(confirmationData.getOperationId())
+                .thenReturn(opId);
+        Mockito.when(confirmationData.getCode())
+                .thenReturn(code);
+        Mockito.when(operationIdRepositoryImpl.putCode(opId, code))
+                .thenReturn(true);
         //then:
         Assertions.assertDoesNotThrow(() -> {
             //when:
-            moneyTransferServiceImpl.makeTransfer(transferData);
+            moneyTransferServiceImpl.confirmOperation(confirmationData);
         });
     }
-//TODO: Fix test for new method code
-//    @Test
-//    public void testConfirmOperation_whenCodeIsNull_thenThrowException() {
-//        //given:
-//        Code code = Mockito.mock(Code.class);
-//        Mockito.when(code.getCode())
-//                .thenReturn(null);
-//        //then:
-//        Assertions.assertThrows(OperationNotConfirmed.class, () -> {
-//            //when:
-//            moneyTransferService.confirmOperation(code);
-//        });
-//    }
-//TODO: Fix test for new method code
-//    @Test
-//    public void testConfirmOperation_whenCodeIsNotNull_thenDontThrowException() {
-//        //given:
-//        Code code = Mockito.mock(Code.class);
-//        Mockito.when(code.getCode())
-//                .thenReturn("0000");
-//        //then:
-//        Assertions.assertDoesNotThrow(() -> {
-//            //when:
-//            moneyTransferService.confirmOperation(code);
-//        });
-//    }
+
+    @Test
+    public void testConfirmOperation_whenPutCodeReturnsFalse_thenThrowException() {
+        //given:
+        OperationId opId = new OperationId("111");
+        Code code = new Code("0000");
+        ConfirmationData confirmationData = Mockito.mock(ConfirmationData.class);
+        Mockito.when(confirmationData.getOperationId())
+                .thenReturn(opId);
+        Mockito.when(confirmationData.getCode())
+                .thenReturn(code);
+        Mockito.when(operationIdRepositoryImpl.putCode(opId, code))
+                .thenReturn(false);
+        //then:
+        Assertions.assertThrows(OperationNotConfirmed.class, () -> {
+            //when:
+            moneyTransferServiceImpl.confirmOperation(confirmationData);
+        });
+    }
+
+    public static Stream<Arguments> sourceForConfirmOperation() {
+        OperationId nullOpId = null;
+        Code nullCode = null;
+        return Stream.of(Arguments.of(nullCode, new OperationId("1")),
+                Arguments.of(new Code("0000"), nullOpId));
+    }
 }
