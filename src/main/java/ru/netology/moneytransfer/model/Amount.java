23ad99f395724fb8cd280678b935d487b9d43718
em2021 <egor_m@icloud.com>
2023-06-03 00:04:37 +0300

    Added NoArgs constructor to Amount for correct deserialization

--- src/main/java/ru/netology/moneytransfer/model/Amount.java
+++ src/main/java/ru/netology/moneytransfer/model/Amount.java
@@ -4,10 +4,12 @@ package ru.netology.moneytransfer.model;
 import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Amount {
 
     private Integer value;
